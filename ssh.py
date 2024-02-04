b2febd3460b45dbe88d92824e6c85003747dd26c
chris.helms <cjohnhelms@gmail.com>
2024-02-04 12:07:36 -0600

    quiet ssh

--- ssh.py
+++ ssh.py
@@ -1,15 +1,15 @@
 import subprocess
 
 def get_vms(host):
-    result = subprocess.run(['ssh', '-A', '-J', 'jump2,login.cgs', host, 'sudo virsh list --name'], capture_output=True, text=True)
+    result = subprocess.run(['ssh', '-q', '-A', '-J', 'jump2,login.cgs', host, 'sudo virsh list --name'], capture_output=True, text=True)
     return result.stdout.strip().split('\n')
 
 def get_vip(host):
-    result = subprocess.run(['ssh', '-A', '-J', 'jump2,login.cgs', host, "ip a show br0 | grep secondary | awk '{ print $2 }' | awk -F '/' '{ print $1 }'"], capture_output=True, text=True)
+    result = subprocess.run(['ssh','-q', '-A', '-J', 'jump2,login.cgs', host, "ip a show br0 | grep secondary | awk '{ print $2 }' | awk -F '/' '{ print $1 }'"], capture_output=True, text=True)
     return result.stdout.strip()
 
 def get_remote_port(host, vm):
-    result = subprocess.run(['ssh', '-A', '-J', 'jump2,login.cgs', host, f'sudo virsh dumpxml {vm} | grep spice | grep port | cut -d "\'" -f4'], capture_output=True, text=True)
+    result = subprocess.run(['ssh', '-q', '-A', '-J', 'jump2,login.cgs', host, f'sudo virsh dumpxml {vm} | grep spice | grep port | cut -d "\'" -f4'], capture_output=True, text=True)
     return result.stdout.strip()
 
 def start_tunnel(host, vip, remote_port, local_port):
