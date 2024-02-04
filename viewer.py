3eea1e6ebc1267e0aca868bc167276a73bb35a62
chris.helms <cjohnhelms@gmail.com>
2024-02-04 12:05:30 -0600

    use https for rlsi

--- viewer.py
+++ viewer.py
@@ -2,7 +2,7 @@ import webbrowser
 import subprocess
 
 def open_browser(local_port):
-    url = 'http://localhost:' + str(local_port)
+    url = 'https://localhost:' + str(local_port)
     webbrowser.open(url)
 
 def open_remote_viewer(local_port):
