8f264bd619a4062dc0fcb8c23ca16a0882362718
Christopher Helms <cjohnhelms@gmail.com>
2024-02-06 21:26:26 -0600

    no strict host key checking

--- modules/ssh.py
+++ modules/ssh.py
@@ -2,9 +2,9 @@ import subprocess
 
 def execute(host, cmd=''):
     cust = host[:3]
-    result = subprocess.run(['ssh', '-q', '-A', '-J', f'jump2,login.{cust}', host, cmd], capture_output=True, text=True)
+    result = subprocess.run(['ssh', '-o StrictHostChecking=no', '-q', '-A', '-J', f'jump2,login.{cust}', host, cmd], capture_output=True, text=True)
     return result
 
 def start_tunnel(host, vip, remote_port, local_port):
     cust = host[:3]
-    subprocess.Popen(['ssh', '-AL', f'{local_port}:{vip}:{remote_port}', '-J', f'jump2,login.{cust}', host], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.Popen(['ssh', '-AL', '-o StrictHostChecking=no', f'{local_port}:{vip}:{remote_port}', '-J', f'jump2,login.{cust}', host], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
