7d1fe88752a7e96f9ebfb9c0104ff101ddf141b8
Christopher Helms <cjohnhelms@gmail.com>
2024-02-07 12:53:16 -0600

    ssh timeout increase

--- modules/ssh.py
+++ modules/ssh.py
@@ -2,9 +2,9 @@ import subprocess
 
 def execute(host, cmd=''):
     cust = host[:3]
-    result = subprocess.run(['ssh', '-o', 'StrictHostKeyChecking=no', '-o', 'ConnectTimeout=10', '-q', '-A', '-J', f'jump2,login.{cust}', host, cmd], capture_output=True, text=True)
+    result = subprocess.run(['ssh', '-o', 'StrictHostKeyChecking=no', '-o', 'ConnectTimeout=60', '-q', '-A', '-J', f'jump2,login.{cust}', host, cmd], capture_output=True, text=True)
     return result
 
 def start_tunnel(host, vip, remote_port, local_port):
     cust = host[:3]
-    subprocess.Popen(['ssh', '-AL', f'{local_port}:{vip}:{remote_port}', '-o', 'StrictHostChecking=no', '-o', 'ConnectTimeout=10', '-J', f'jump2,login.{cust}', host], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.Popen(['ssh', '-AL', f'{local_port}:{vip}:{remote_port}', '-o', 'StrictHostChecking=no', '-o', 'ConnectTimeout=60', '-J', f'jump2,login.{cust}', host], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
