c8568d47b4167569db08ac78d4e9a69459daf915
Christopher Helms <cjohnhelms@gmail.com>
2024-02-07 16:28:44 -0600

    add tmp ssh config

--- modules/ssh.py
+++ modules/ssh.py
@@ -1,10 +1,41 @@
 import subprocess
+import os
+
+def mkconfig(host):
+    cust = host[:3]
+    tmp = '.ssh/tmp/'
+    file_path = '.ssh/tmp/config'
+    file_contents = f"""
+    Host *
+      ControlMaster auto
+      ControlPersist 8h
+      ServerAliveInterval 100
+      ControlPath ~/.ssh/control/%h-%p-%r
+      StrictHostKeyChecking no
+      ForwardAgent yes
+      IdentityFile ~/.ssh/id_rsa
+      AddKeysToAgent yes
+      PubkeyAcceptedKeyTypes +ssh-rsa
+      HostKeyAlgorithms +ssh-dss,ssh-rsa
+    Host login*
+      ProxyJump jump2
+    Host {cust}*
+      ProxyJump login.{cust}
+    """
+    os.makedirs(tmp)
+    f = open(file_path, 'a')
+    f.write(file_contents)
+
+def rmconfig():
+    subprocess.run(['rm', '-rf', '.ssh'])
 
 def execute(host, cmd=''):
-    cust = host[:3]
-    result = subprocess.run(['ssh', '-o', 'StrictHostKeyChecking=no', '-o', 'ConnectTimeout=60', '-q', '-A', '-J', f'jump2,login.{cust}', host, cmd], capture_output=True, text=True)
+    mkconfig(host)
+    result = subprocess.run(['ssh', '-F', '.ssh/tmp/config', '-o', 'ConnectTimeout=60', '-q', host, cmd], capture_output=True, text=True)
+    rmconfig()
     return result
 
 def start_tunnel(host, vip, remote_port, local_port):
-    cust = host[:3]
-    subprocess.Popen(['ssh', '-AL', f'{local_port}:{vip}:{remote_port}', '-o', 'StrictHostChecking=no', '-o', 'ConnectTimeout=60', '-J', f'jump2,login.{cust}', host], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    mkconfig(host)
+    subprocess.Popen(['ssh', '-F', '.ssh/tmp/config', '-AL', f'{local_port}:{vip}:{remote_port}', '-o', 'ConnectTimeout=60', host], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    rmconfig()
